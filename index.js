16e071d58b43c6f0801a195feb381d10c7d2f066
Macbook <kyle.poul@gmail.com>
2017-02-08 16:09:21 -0700

    node practice

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
+var express = require('express')
+var bodyParser = require('body-parser');
 
+
+var messages = [
+
+
+]
+var app = express();
+
+
+app.use(express.static('assets'));
+app.use(bodyParser.json());
+
+app.get('/messages', function(req, res, next){
+  res.status(200).json({ messages: messages });
+})
+
+app.post('/messages', function(req, res, next){
+  messages.push({name: req.body.message, message: req.body.message, time: new Date()});
+  res.status(200).json({ messages: messages })
+})
+
+
+var port = 3000
+app.listen(port, function(){
+console.log("listening on port " + port);
+});
